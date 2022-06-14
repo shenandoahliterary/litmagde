fcf9661f17fb0d63f4e550c3762421a2d24736bf
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2022-06-14 09:31:34 -0400

    Update litmagde-plugin.php
    Add code template and slightly modified.

--- litmagde-plugin.php
+++ litmagde-plugin.php
@@ -7,7 +7,31 @@ Author: Jeff Barry
 Author URI: https://endlesshybrids.com
 */
 /* Start Adding Functions Below this Line */
-  
+function wporg_register_taxonomy_TOC() {
+    $labels = array(
+        'name'              => _x( 'TOC', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Course', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Courses' ),
+        'all_items'         => __( 'All Courses' ),
+        'parent_item'       => __( 'Parent Course' ),
+        'parent_item_colon' => __( 'Parent Course:' ),
+        'edit_item'         => __( 'Edit Course' ),
+        'update_item'       => __( 'Update Course' ),
+        'add_new_item'      => __( 'Add New Course' ),
+        'new_item_name'     => __( 'New Course Name' ),
+        'menu_name'         => __( 'Course' ),
+    );
+    $args   = array(
+        'hierarchical'      => true, // make it hierarchical (like categories)
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => [ 'slug' => 'course' ],
+    );
+    register_taxonomy( 'TOCorder', [ 'post' ], $args );
+}
+add_action( 'init', 'wporg_register_taxonomy_TOC' );  
   
 /* Stop Adding Functions Below this Line */
 ?>
