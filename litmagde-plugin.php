d809cabb298bca803621d4aadba61bb77d8cd251
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2022-06-14 09:21:47 -0400

    Update litmagde-plugin.php
    Added more metadata

--- litmagde-plugin.php
+++ litmagde-plugin.php
@@ -2,6 +2,9 @@
 /*
 Plugin Name: LitMagDe
 Description: Literary Magazine Design, a plugin for adding functionalty to Shenandoah literary magazine.
+Version: 0.1
+Author: Jeff Barry
+Author URI: https://endlesshybrids.com
 */
 /* Start Adding Functions Below this Line */
   
