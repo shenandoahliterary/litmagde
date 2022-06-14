3e596339b63ca639a2d12f04ff9239e1981a9f0f
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2022-06-14 09:13:10 -0400

    Create litmagde-plugin.php
    Create basic plugin structure.

---
+++ litmagde-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: LitMagDe
+Description: Literary Magazine Design, a plugin for adding functionalty to Shenandoah literary magazine.
+*/
+/* Start Adding Functions Below this Line */
+  
+  
+/* Stop Adding Functions Below this Line */
+?>
